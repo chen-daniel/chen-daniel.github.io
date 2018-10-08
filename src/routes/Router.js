fe050b860ed677042bd746556fcab795c61b1347
Daniel Chen <chen.daniel@husky.neu.edu>
2018-10-07 21:39:38 -0400

    Setup router

---
+++ src/routes/Router.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { BrowserRouter, Route, Link, Switch } from "react-router-dom";
+
+import './Router.css';
+import Home from './Home/Home.component';
+import About from './About/About.component';
+import Resume from './Resume/Resume.component';
+import Projects from './Projects/Projects.component';
+import Photography from './Photography/Photography.component';
+
+export default () => (
+  <BrowserRouter>
+    <Switch>
+      <Route exact path="/" component={Home} />
+      <Route exact path="/about" component={About} />
+      <Route exact path="/resume" component={Resume} />
+      <Route exact path="/projects" component={Projects} />
+      <Route exact path="/photography" component={Photography} />
+    </Switch>
+  </BrowserRouter>
+);
