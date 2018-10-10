597e6736b5137da81088c2dbd0fd51a6df67059c
Daniel Chen <chen.daniel@husky.neu.edu>
2018-10-09 22:04:20 -0400

    Main Page Complete

--- src/common-components/Navbar/navbar.component.js
+++ src/common-components/Navbar/navbar.component.js
@@ -4,43 +4,57 @@ import { HashRouter } from 'react-router-dom';
 
 import './navbar.css';
 
-export default () => (
-  <HashRouter>
-    <nav id="nav">
-      <ul>
-        <li className="current">
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/about">About</Link>
-        </li>
-        <li>
-          <Link to="/resume">Resume</Link>
-        </li>
-        <li>
-          <Link to="/projects">Projects</Link>
-        </li>
-        <li>
-          <Link to="/photography">Photography</Link>
+class Navbar extends React.Component {
+  componentDidMount() {
+    let s = document.createElement('script');
+    s.src = 'assets/js/main.js';
+    s.type = 'text/javascript';
+    s.async = true;
+    document.body.appendChild(s);
+  }
+
+  render() {
+    return (
+      <HashRouter>
+        <nav id="nav">
           <ul>
             <li>
-              <Link to="/photography/portrait">Portrait</Link>
+              <Link to="/">Home</Link>
             </li>
             <li>
-              <Link to="/photography/street_urban">Street/Urban</Link>
+              <Link to="/about">About</Link>
             </li>
             <li>
-              <Link to="/photography/landscape">Landscape</Link>
+              <Link to="/resume">Resume</Link>
             </li>
             <li>
-              <Link to="/photography/event">Event</Link>
+              <Link to="/projects">Projects</Link>
             </li>
             <li>
-              <Link to="/photography/cosplay">Cosplay</Link>
+              <Link to="/photography">Photography</Link>
+              <ul>
+                <li>
+                  <Link to="/photography/portrait">Portrait</Link>
+                </li>
+                <li>
+                  <Link to="/photography/street_urban">Street/Urban</Link>
+                </li>
+                <li>
+                  <Link to="/photography/landscape">Landscape</Link>
+                </li>
+                <li>
+                  <Link to="/photography/event">Event</Link>
+                </li>
+                <li>
+                  <Link to="/photography/cosplay">Cosplay</Link>
+                </li>
+              </ul>
             </li>
           </ul>
-        </li>
-      </ul>
-    </nav>
-  </HashRouter>
-);
+        </nav>
+      </HashRouter>
+    );
+  }
+}
+
+export default Navbar;
