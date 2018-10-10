3acc0d0a48e17b9b2c49e32e7237dfdd604949c0
Daniel Chen <chen.daniel@husky.neu.edu>
2018-10-09 21:00:40 -0400

    Finally setup page layout

--- src/App.js
+++ src/App.js
@@ -6,15 +6,15 @@ import Navbar from './common-components/Navbar/navbar.component';
 import './App.css';
 
 export default () => (
-  <div>
+  <div id="wrapper">
     <Navbar />
     <div>
       <Router />
-      <footer id="footer">
-        <ul className="copyright">
-          <li>&copy; Daniel Chen</li>
-        </ul>
-      </footer>
     </div>
+    <footer id="footer">
+      <ul className="copyright">
+        <li>&copy; Daniel Chen</li>
+      </ul>
+    </footer>
   </div>
 );
