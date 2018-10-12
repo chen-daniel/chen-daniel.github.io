13996448a59f39d5deefd147c2b7d60b2b84686f
Daniel Chen <chen.daniel@husky.neu.edu>
2018-10-12 14:53:24 -0400

    edit about page, fix margins for photography page

--- src/routes/About/About.component.js
+++ src/routes/About/About.component.js
@@ -20,24 +20,24 @@ class About extends React.Component {
         </header>
         <p>
           Hi! My name is Daniel and I'm currently a Computer Science student at
-          Northeastern University with a minor in Entrepreneurship. I was born
-          in Canada, grew up in Hong Kong, and studied for a year in Canada
-          before moving to Boston. Coding has been a passion of mine since I
-          attended my first university lecture. Since then I've had the
-          opportunity to continuously grow and develop in the field. My
-          specialties at the moment are in full-stack web development. Through
-          classes, hackathons, and work, I've been given exposure to a myriad of
-          web technologies and frameworks.
+          Northeastern University with a minor in Entrepreneurship. I'm
+          passionate about building, iterating upon, and optimizing solutions. I
+          was born in Canada, grew up in Hong Kong, and studied for a year back
+          in Canada before moving to Boston. My interest in computer science was
+          solidified during my first university lecture and since then I've had
+          the opportunity to continuously grow and develop in the field. My
+          interests currently lie in full-stack web development, AI, distributed
+          systems, game development, and robotics.
           <br />
           <br />
           Most recently I was employed as a Software Developer Co-op at Circle.
-          At this position, I have had the opportunity to work with an extremely
-          experienced and talented team of developers who have all helped me
-          improve as both a developer and a member of a team.
+          At this position, I have had the privilege of working with an
+          extremely experienced and talented group of developers who have helped
+          me improve as both a developer and a team member.
           <br />
           <br />
-          Outside of Visual Studio Code, I also enjoy photography (Former Canon
-          user converted to Sony), film, keeping up with tech news, gaming (Xbox
+          Outside of VS Code, I also enjoy photography (Former Canon user
+          converted to Sony), film, keeping up with tech news, gaming (Xbox
           FTW), basketball (Go Lebron), and hip-hop dance (WOO Kinjaz).
         </p>
       </section>
