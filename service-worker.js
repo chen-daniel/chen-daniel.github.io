0e79f4cf752bb2eb311adb84baa2203829a5a4f3
Daniel Chen <chen.daniel@husky.neu.edu>
2018-10-13 04:58:26 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.93737b9309c17a5a22d821f4ff5212f1.js"
+  "/precache-manifest.0443b2ad97c7c3a39834ba5c694b254b.js"
 );
 
 workbox.clientsClaim();
