f9e2294edc15902536d0f4c854243accf924023a
Daniel Chen <chen.daniel@husky.neu.edu>
2018-10-15 01:24:26 -0400

    Updates

--- precache-manifest.fdfd6a8aad775e20b63ccc395311677c.js
+++ precache-manifest.fdfd6a8aad775e20b63ccc395311677c.js
@@ -4,12 +4,12 @@ self.__precacheManifest = [
     "url": "/static/media/bg.30fa517c.jpg"
   },
   {
-    "revision": "c18e0e846bf9e29f23ba",
-    "url": "/static/css/main.c73406b4.chunk.css"
+    "revision": "537a7b3f7c959d650a71",
+    "url": "/static/css/main.4091512b.chunk.css"
   },
   {
-    "revision": "6cf605e7f5efacb671b9",
-    "url": "/static/js/1.6cf605e7.chunk.js"
+    "revision": "b8dac9aa0b02562d4970",
+    "url": "/static/js/1.b8dac9aa.chunk.js"
   },
   {
     "revision": "229c360febb4351a89df",
@@ -24,8 +24,8 @@ self.__precacheManifest = [
     "url": "/static/media/profile.5b87dcf2.jpg"
   },
   {
-    "revision": "c18e0e846bf9e29f23ba",
-    "url": "/static/js/main.c18e0e84.chunk.js"
+    "revision": "537a7b3f7c959d650a71",
+    "url": "/static/js/main.537a7b3f.chunk.js"
   },
   {
     "revision": "e6cf7c6ec7c2d6f670ae9d762604cb0b",
@@ -48,7 +48,7 @@ self.__precacheManifest = [
     "url": "/static/media/fontawesome-webfont.d7c63908.svg"
   },
   {
-    "revision": "612c604fbe8003093fb0e0645d74ddec",
+    "revision": "cf245a16d85d1aeb9fcb4ac4248611a9",
     "url": "/index.html"
   }
 ];
